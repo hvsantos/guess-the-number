fa047d876040a9ef53de4b60e85887c66c95a65f
Henrique Santos <hv_santos@outlook.com>
2022-03-10 20:25:27 -0300

    refactor

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ const $input = document.querySelector('.inputguess')
 const $try = document.querySelector('#trybutton')
 const $answer = document.querySelector('.answer')
 const $reset = document.querySelector('#resetbutton')
+const $trychances = document.querySelector('.trychances')
 
 $try.addEventListener('click', clickExec)
 $reset.addEventListener('click', resetGuess)
 
 let secretValue = parseInt(Math.random() * 11)
 let guessTries = 0
-
+let totalTry = 3
+let finTry = 0
 function clickExec(event) {
   event.preventDefault()
   let valueGuess = Number($input.value)
@@ -20,6 +22,8 @@ function clickExec(event) {
     } else {
       $answer.textContent = 'Favor insira um valor entre 0 e 10'
     }
+    finTry = totalTry - guessTries
+    $trychances.textContent = finTry
   } else {
     $answer.textContent = 'Clique em NOVO NÚMERO para começar novamente'
   }
@@ -29,18 +33,12 @@ function guessItAll(value) {
     $answer.textContent = 'Parabéns, você acertou, o número era ' + secretValue
     guessTries = 3
   } else {
-    let totalTry = 3
-    totalTry = totalTry - guessTries
     if (value > secretValue) {
       $answer.textContent =
-        'Você errou, o número inserido é maior que o número secreto, você ainda possui ' +
-        totalTry +
-        ' chances'
+        'Você errou, o número inserido é maior que o número secreto'
     } else if (value < secretValue) {
       $answer.textContent =
-        'Você errou, o número inserido é menor que o número secreto ' +
-        totalTry +
-        ' chances'
+        'Você errou, o número inserido é menor que o número secreto'
     }
   }
 }
@@ -48,5 +46,7 @@ function resetGuess(event) {
   event.preventDefault()
   secretValue = parseInt(Math.random() * 11)
   guessTries = 0
-  console.log(guessTries)
+  totalTry = 3
+  finTry = 0
+  $trychances.textContent = 3
 }
